5041926da8e7a08dfd58a72e1692271c4d0b12cf
Anibal <juananibal.micheli@gmail.com>
2023-05-25 12:43:55 -0300

    Update dreambooth.py

--- dreambooth.py
+++ dreambooth.py
@@ -779,7 +779,7 @@ def main(args):
                 text_enc_model = CLIPTextModel.from_pretrained(
                     args.pretrained_model_name_or_path, subfolder="text_encoder", revision=args.revision)
             scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012,
-                                      beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False)
+                                      beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False, steps_offset=1)
             pipeline = StableDiffusionInpaintPipeline.from_pretrained(
                 args.pretrained_model_name_or_path,
                 unet=accelerator.unwrap_model(
